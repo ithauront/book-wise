fb61fe174b1bb4274d2c83f921a6f793ad6dfea2
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-09-27 08:47:11 +0200

    [feat]: avatar component + move assets to public

--- src/components/Avatar/Avatar.component.tsx
+++ src/components/Avatar/Avatar.component.tsx
@@ -7,10 +7,14 @@ import {
 } from './styles'
 import { User } from 'phosphor-react'
 
-export function Avatar(props: ComponentProps<typeof AvatarImage>) {
+interface AvatarProps extends ComponentProps<typeof AvatarImage> {
+  size?: 'sm' | 'md' | 'lg'
+}
+
+export function Avatar({ size, ...props }: AvatarProps) {
   return (
     <AvatarBorder>
-      <AvatarContainer>
+      <AvatarContainer size={size}>
         <AvatarImage {...props} />
         <AvatarFallback delayMs={600}>
           <User />
--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,23 +1,41 @@
 import * as Avatar from '@radix-ui/react-avatar'
-import { styled, theme } from '../../stitches.config'
+import { styled } from '../../styles'
 
 export const AvatarBorder = styled('div', {
-    display: 'inline-block',
-    background: theme.colors['gradient-vertical'],
-    borderRadius: '$full',
-    padding: theme.space[1], 
-  })
+  display: 'inline-block',
+  background: '$gradient-vertical',
+  borderRadius: '$full',
+  padding: '2px',
+})
 
 export const AvatarContainer = styled(Avatar.Root, {
   borderRadius: '$full',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: '$16',
-  height: '$16',
   overflow: 'hidden',
-  background:  theme.colors['gray-800']
+  background: '$gray-800',
 
+  variants: {
+    size: {
+      sm: {
+        width: '1.5rem',
+        height: '1.5rem',
+      },
+      md: {
+        width: '2.5rem',
+        height: '2.5rem',
+      },
+      lg: {
+        width: '4.5rem',
+        height: '4.5rem',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
 })
 
 export const AvatarImage = styled(Avatar.Image, {
