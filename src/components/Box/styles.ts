18b2704b974d00b986b6973d5247881080896b24
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-10-15 14:27:32 +0200

    [style](Box): added box variants and upgrade style for box

--- src/components/Box/styles.ts
+++ src/components/Box/styles.ts
@@ -1,7 +1,25 @@
 import { styled } from '../../styles'
 
 export const BoxComponent = styled('div', {
-  height: '100vh',
-  width: '14.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+
+  background: '$gray-700',
+
   borderRadius: '$md',
+
+  variants: {
+    backgroundColor: {
+      regular: {
+        background: '$gray-700',
+      },
+      clear: {
+        background: '$gray-600',
+      },
+    },
+  },
+
+  defaultVariants: {
+    backgroundColor: 'regular',
+  },
 })
