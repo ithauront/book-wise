4807fe0142e88cf838e20b7b1d89db90b5a3c5a5
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-10-29 13:05:37 +0100

    [feat]: list ratings api

---
+++ src/pages/api/list-ratings.api.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../lib/prisma'
+import { Rating } from '../types'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse<{ allRatings: Rating[] } | { error: string }>,
+) {
+  try {
+    const allRatings = await prisma.rating.findMany({
+      select: {
+        id: true,
+        rate: true,
+        description: true,
+        created_at: true,
+        book: {
+          select: {
+            id: true,
+            name: true,
+            author: true,
+            summary: true,
+            cover_url: true,
+          },
+        },
+        user: {
+          select: {
+            name: true,
+            avatar_url: true,
+          },
+        },
+      },
+    })
+    res.status(200).json({ allRatings })
+  } catch (error) {
+    res.status(500).json({ error: `Erro ao buscar avaliações: ${error}` })
+  }
+}
