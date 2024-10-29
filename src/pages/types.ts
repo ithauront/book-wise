b0f59b7edf859362a83049bd4146dccff7d1a10c
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-10-29 13:05:18 +0100

    [feat]: made shared types file

--- src/pages/types.ts
+++ src/pages/types.ts
@@ -1,15 +1,20 @@
+export interface User {
+  name: string | null
+  avatar_url: string | null
+}
+
 export interface Category {
   id: string
   name: string
 }
-
 export interface Rating {
   id: string
   rate: number
   description: string
   created_at: Date
-  book_id: string
-  user_id: string
+  // eslint-disable-next-line no-use-before-define
+  book?: Book
+  user?: User
 }
 
 export interface Book {
@@ -18,8 +23,8 @@ export interface Book {
   author: string
   summary: string
   cover_url: string
-  total_pages: number
-  created_at: Date
-  ratings: Rating[]
-  categories: { category: Category }[]
+  total_pages?: number
+  created_at?: Date
+  ratings?: Rating[]
+  categories?: { category: Category }[]
 }
