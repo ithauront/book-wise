0db18a8f0a3c6debd5c6a51d6b5e860fd165ccd1
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-11-10 16:01:55 +0100

    [feat]: comment close on button and maxLength validation

--- src/components/TextInput/TextInput.component.tsx
+++ src/components/TextInput/TextInput.component.tsx
@@ -11,20 +11,26 @@ import { useForm } from 'react-hook-form'
 interface TextInputProps {
   placeholder: string
   onSubmit: (data: { comment: string }) => void
+  onClose: () => void
 }
-// TODO fazer os butões de enviar e o x para fexar o comentario.
-// TODO fazer a validação do tamanho do input, e colocar o charcounter para contar corretamente
-export function TextInput({ onSubmit, placeholder }: TextInputProps) {
-  const { register, handleSubmit } = useForm<{ comment: string }>()
+// TODO no issubmiting desabilitar o butão de envio. apos submitar talvez dar uma msg de seu comentario foi bem registrado. e fechar a aba de comentario. ou apenas fechar a aba e ostrar o comentario novo na lista.
+
+export function TextInput({ onSubmit, placeholder, onClose }: TextInputProps) {
+  const { register, handleSubmit, watch } = useForm<{ comment: string }>()
+  const commentValue = watch('comment') || ''
   return (
     <ContainerForm onSubmit={handleSubmit(onSubmit)}>
-      <CommentInput placeholder={placeholder} {...register('comment')} />
-      <CharCounter>0/300</CharCounter>
+      <CommentInput
+        maxLength={450}
+        placeholder={placeholder}
+        {...register('comment')}
+      />
+      <CharCounter>{`${commentValue.length}/450`}</CharCounter>
       <ButtonsContainer>
-        <IconButton>
+        <IconButton onClick={onClose}>
           <X size={24} />
         </IconButton>
-        <IconButton variant="submit">
+        <IconButton variant="submit" type="submit">
           <Check size={24} />
         </IconButton>
       </ButtonsContainer>
